0092b9bbd3160cd2f7719a5aa6cc05d5055bc57d
sofritaa <129680402+sofritaa@users.noreply.github.com>
2023-05-24 11:33:27 -0400

    dia 3

---
+++ Dia3/blucles/dia3.js
@@ -0,0 +1,31 @@
+//ejercicio 1//
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+//ejercicio 2//
+for (var i = 100; i <= 0; i--) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
+//ejercicio 3//
+var num = 4;
+while (num >= -3.5) {
+    console.log(num);
+    num -= 1.5;
+}
+//ejercicio 4// 
+var sum = 0;
+for (var i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log(sum);
+
+//ejercicio 5//
+var product = 1;
+for (var i = 1; i <= 12; i++) {
+    product *= i;
+}
+console.log(product);
