7d7ad61c9fb974886afbbd0e923df5f9b942ad32
sofritaa <129680402+sofritaa@users.noreply.github.com>
2023-05-23 20:58:04 -0400

    Buzzfizz

---
+++ fizzbuzz/FizzBuzz.js
@@ -0,0 +1,11 @@
+for (var num = 1; num < 101; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+}
